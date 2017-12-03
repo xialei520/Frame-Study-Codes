08a8ebe974e135bbd7aa854a7b5afd4fc4f785e2
夏磊 <377284071@qq.com>
2017-12-03 22:56:15 +0800

    11.7

---
+++ 11.7-vue/myday7/webpack/js/tool.js
@@ -0,0 +1,4 @@
+function aaa(a, b){
+	return a+ b;
+}
+module.exports = aaa;
---
+++ 11.7-vue/myday7/webpack/vue/header.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<header>{{name}}</header>
+	</div>
+	
+</template>
+
+<script>
+//	module.exports={}
+//ES6写法
+	export default {
+		data(){
+			return{
+				name:"头部组件"
+			}
+			
+		}
+	}
+</script>
+
+<style scoped>
+	/*scoped 将样式锁在本地，这样就不会影响外面的样式*/
+	header{
+		
+	}
+</style>
